0f8e8ac70bb0f384b8fe57e6dd016bf5dbbac822
Dullestdear <129474954+Dullestdear@users.noreply.github.com>
2026-09-02 21:34:23 +0530

    Implemtation of a Grid Spawning system

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,12 +1,40 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Variables deciding the dimension for the grid.
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+
+
+    // Reference to Grid Tiles
+    [SerializeField] private Tile tile;
+
+    
     void Start()
     {
-        
+        GenerateGrid(); // Calling the function to spawn grid
     }
+
+    //Function to spawn/generate the grid using x and z axis
+
+    void GenerateGrid()
+    {
+        for(int x =0 ; x < width ; x++)
+        {
+            for(int z =0 ; x < height ; z++)
+            {
+                // Spawning the tiles
+                var SpawnedTile = Instantiate(tile , new Vector3(x,z), Quaternion.identity);
+
+                // Naming the Tiles
+                SpawnedTile.name = $"Tile {x} {z}";
+            }
+        }
+    }
+
+    
 
     // Update is called once per frame
     void Update()
