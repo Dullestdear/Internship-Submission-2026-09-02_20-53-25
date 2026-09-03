1b25a23da465e18a0329d79232d3ba8e0a703440
Dullestdear <129474954+Dullestdear@users.noreply.github.com>
2026-09-03 22:35:34 +0530

    Cleanup

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class EnemyAI : MonoBehaviour, AIInterface
 {
-    [SerializeField] private float speed = 5f;
     public bool isMoving{ get; private set;}
 
     //Player and Grid 
@@ -29,19 +28,23 @@ public class EnemyAI : MonoBehaviour, AIInterface
 
     void Update()
     {
-        //Code for making the enemy look at the player
-        Vector3 directionToPlayer =(player.transform.position-transform.position).normalized;
-        
-        //locking the Y axis to prevent any form of tilts and movement based errors
-
-        directionToPlayer.y =0;
-
-        if(directionToPlayer!= Vector3.zero)
+        if (isMoving)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
-            transform.rotation= Quaternion.Slerp(transform.rotation,targetRotation
-            ,5f*Time.deltaTime);
-        } 
+            //Code for making the enemy look at the player
+            Vector3 directionToPlayer =(player.transform.position-transform.position).normalized;
+            
+            //locking the Y axis to prevent any form of tilts and movement based errors
+
+            directionToPlayer.y =0;
+
+            if(directionToPlayer!= Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+                transform.rotation= Quaternion.Slerp(transform.rotation,targetRotation
+                ,5f*Time.deltaTime);
+            }            
+        }
+        
     }
 
 
@@ -89,27 +92,58 @@ public class EnemyAI : MonoBehaviour, AIInterface
 
     private IEnumerator Move(List<Tile> path)
     {
-        isMoving = true; // did this to make sure that the enemy is currently moving
+        isMoving = true; // did this to make sure that the player is currently moving
 
         // Code to go through each tile one by one
         foreach (Tile targetTile in path)
         {
+
+            //Animation Start Pos
+            Vector3 startPos=transform.position;
+
             // spawning 0.1f above so that the player doesnt spawn inside the tile
             Vector3 targetPosition = targetTile.transform.position+ new Vector3(0,0.1f,0);
 
-            while (Vector3.Distance(transform.position, targetPosition) > 0.01f)
+            //Animation Var
+            float jumpTime = 0.3f;
+            float jumpHeight = 0.5f;
+            float elapsedTime = 0f;
+
+            while (elapsedTime<jumpTime)
             {
-                transform.position = Vector3.MoveTowards(transform.position , targetPosition 
-                , speed*Time.deltaTime);
+                elapsedTime += Time.deltaTime;
+                float percentage = elapsedTime/jumpTime;
+
+                //Rotation based on where the player the going
+                Vector3 MovingDirect = (targetPosition - transform.position).normalized;
+
+                // No SLANT BUG
+                MovingDirect.y = 0f;
+                
+                if (MovingDirect!= Vector3.zero)
+                {
+                    Quaternion targetRotation = Quaternion.LookRotation(MovingDirect);
+                    transform.rotation= Quaternion.Slerp(transform.rotation,targetRotation
+                    ,10f*Time.deltaTime);
+                }
+
+                //Hop movment code using sine
+                Vector3 latestPos= Vector3.Lerp(startPos, targetPosition,percentage);
+                latestPos.y += Mathf.Sin(percentage*Mathf.PI)*jumpHeight;
+                transform.position=latestPos;
+
+
+                
                 yield return null;
             }
 
             // Exact Position in integers
             transform.position = targetPosition;
 
-            //new tile coords
+            // New Tile Coords
             currentX = targetTile.gridX;
             currentZ = targetTile.gridZ;
+
         }
         isMoving = false;
     }
